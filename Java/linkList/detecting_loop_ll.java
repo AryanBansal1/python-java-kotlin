33f216cdeec32d4cfcfca0399bca64c1bf5155e7
Aryan <aryanbansalaryan@gmail.com>
2024-09-17 13:52:09 +0530

    cycle in ll

--- Java/linkList/detecting_loop_ll.java
+++ Java/linkList/detecting_loop_ll.java
@@ -38,6 +38,29 @@ public class detecting_loop_ll {
             return false;
         }
 
+        public void removeLoop(){
+            Node slow = head;
+            Node fast = head;
+            Node prev = null;
+            while(fast !=null && fast.next !=null){
+                slow = slow.next;
+                fast = fast.next.next;
+                if(slow == fast){
+                    slow =head;
+                    break;
+                }
+               
+            }
+            System.out.println("the point of meeting " + fast.data);
+            while(slow != fast){
+                slow = slow.next;
+                prev = fast;
+                fast = fast.next;
+            }
+            System.out.println("the last element is "+ prev.data);
+            prev.next = null;
+        }
+
         public void print(){
             Node temp = head;
             while(temp !=null){
@@ -52,11 +75,13 @@ public class detecting_loop_ll {
     public static void main(String[] args) {
         detecting_loop_ll ll = new detecting_loop_ll();
         head = new Node(5);
-        head.next = new Node(10);
-        head.next.next = new Node(15);
-        head.next.next.next = head;
-        
-        //ll.print();
-        System.out.println(ll.detect_loop());
+        head.next = new Node(9);
+        head.next.next = new Node(28);
+        head.next.next.next = new Node(57);
+        head.next.next.next.next = new Node(12);
+        head.next.next.next.next.next = new Node(44);
+        head.next.next.next.next.next.next = head.next.next;
+        ll.removeLoop();
+        ll.print();
     }
 }
